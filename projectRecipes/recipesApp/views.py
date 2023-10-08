cceb915b3fe894551cac69b00177c9a25d9aaeaf
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-08 17:33:17 -0300

    #

--- projectRecipes/recipesApp/views.py
+++ projectRecipes/recipesApp/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    context = {'nome': 'Lucas'}
-    return render(request, 'global/home.html', context)
+    return render(request, 'global/home.html')
