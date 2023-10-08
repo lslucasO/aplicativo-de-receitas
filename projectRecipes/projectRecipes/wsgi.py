8d8580cf657e8d37f09ca9dd1ffbb378ebebd31d
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-07 22:51:58 -0300

    #

--- projectRecipes/projectRecipes/wsgi.py
+++ projectRecipes/projectRecipes/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projetoHelloWorld project.
+WSGI config for projectRecipes project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetoHelloWorld.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectRecipes.settings')
 
 application = get_wsgi_application()
--- projectRecipes/recipesApp/apps.py
+++ projectRecipes/recipesApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MeuappConfig(AppConfig):
+class RecipesappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'meuapp'
+    name = 'recipesApp'
