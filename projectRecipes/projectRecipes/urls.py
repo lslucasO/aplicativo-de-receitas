3ac8c677a00a2dac2030e72a47285e5f9e385143
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-08 16:59:55 -0300

    #

--- projectRecipes/projectRecipes/urls.py
+++ projectRecipes/projectRecipes/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from recipesApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home")
+    path('', include('recipesApp.urls'))
 ]
