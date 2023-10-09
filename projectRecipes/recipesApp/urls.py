94c2278430cd455075d38676e12d85e285f7a28d
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-08 22:44:38 -0300

    #

--- projectRecipes/recipesApp/urls.py
+++ projectRecipes/recipesApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from recipesApp import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('recipe/', views.recipe, name="recipe")
 ]
--- projectRecipes/recipesApp/views.py
+++ projectRecipes/recipesApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'recipesApp/pages/home.html')
+
+def recipe(request):
+    return render(request, 'recipesApp/pages/recipe.html')
