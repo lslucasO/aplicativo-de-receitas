0d7baca6481fe1912e9ca22fe05ac0b412439604
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-10 10:01:55 -0300

    #

--- projectRecipes/projectRecipes/settings.py
+++ projectRecipes/projectRecipes/settings.py
@@ -45,7 +45,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'base_templates')
             ],
         'APP_DIRS': True,
         'OPTIONS': {
