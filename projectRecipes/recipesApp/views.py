ac4674eacd0e5befc436af84931dc730554bf9df
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-12 11:37:55 -0300

    #

--- projectRecipes/recipesApp/views.py
+++ projectRecipes/recipesApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from utils.recipesApp.factory import make_recipe
+from django.http import Http404
 from .models import *
 
 
@@ -11,7 +12,22 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    context = {'recipes': recipes}
+    
+    # category_name = getattr(
+    #     getattr(recipes.first(), 'category', None),
+    #     'name',
+    #     'Not found'
+    # )
+    
+    
+    # Tratando erros de página não encontrada
+    if not recipes:
+        raise Http404('Not Found')
+    
+    
+    context = {'recipes': recipes,
+               'title': f'{recipes.first().category.name}'
+    }
     return render(request, 'recipesApp/pages/category.html', context)
 
 
