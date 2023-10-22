ef7222f71c3583cbb543621b4f2d5d1b8c316f57
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-22 20:14:54 -0300

    #

--- projectRecipes/projectRecipes/settings.py
+++ projectRecipes/projectRecipes/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
--- projectRecipes/projectRecipes/wsgi.py
+++ projectRecipes/projectRecipes/wsgi.py
@@ -14,4 +14,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectRecipes.settings')
 load_dotenv()
+
 application = get_wsgi_application()
+app = application
