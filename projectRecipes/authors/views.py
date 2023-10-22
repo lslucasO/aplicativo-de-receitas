8b0074474bb03266e4e6ac48e80657e7db266669
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-22 16:55:51 -0300

    #

--- projectRecipes/authors/views.py
+++ projectRecipes/authors/views.py
@@ -31,5 +31,11 @@ def register_create(request):
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
     
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+        
+        del(request.session['register_form_data'])
+    
    # Redirecionando para a view de registro
     return redirect('register')
