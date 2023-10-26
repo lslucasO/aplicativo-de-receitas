2b8058e753416db052f9a6d5b942633c6532d703
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-25 22:47:09 -0300

    #

--- projectRecipes/recipesApp/admin.py
+++ projectRecipes/recipesApp/admin.py
@@ -4,9 +4,47 @@ from .models import *
 class CategoryAdmin(admin.ModelAdmin):
     ...
     
-    
+
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = [
+        'id',
+        'title',
+        'created_at',
+        'is_published',
+        'author'
+    ]
+    
+    list_display_links = [
+        'title',
+        'created_at'
+    ]
+    
+    search_fields = [
+        'id',
+        'title',
+        'description',
+        'created_at',
+        'slug'
+    ]
+    
+    
+    list_filter = [
+        'category',
+        'author',
+        'is_published',
+        'preparation_steps_is_html'
+    ]
+        
+    
+    list_per_page = 10
+    
+    list_editable = [
+        'is_published'
+    ]
+    
+    ordering = [
+        '-id'
+    ]
     
     
 admin.site.register(Category, CategoryAdmin)
